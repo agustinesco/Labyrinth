0d2c8ab7094ac4ee87f469f0762a4b8f43ef77a5
agustinesco <agustinesco@outlook.es>
2026-02-05 13:26:17 -0300

    Fix duplicate SetValues method in MazeGeneratorConfig

--- Assets/Scripts/Maze/MazeGeneratorConfig.cs
+++ Assets/Scripts/Maze/MazeGeneratorConfig.cs
@@ -69,17 +69,6 @@ namespace Labyrinth.Maze
         }
 
         /// <summary>
-        /// Sets the configuration values programmatically
-        /// </summary>
-        public void SetValues(int mazeWidth, int mazeHeight, int corridorW, float branching)
-        {
-            width = mazeWidth;
-            height = mazeHeight;
-            corridorWidth = corridorW;
-            branchingFactor = branching;
-        }
-
-        /// <summary>
         /// Creates a MazeGenerator instance using this configuration
         /// </summary>
         public MazeGenerator CreateGenerator()
